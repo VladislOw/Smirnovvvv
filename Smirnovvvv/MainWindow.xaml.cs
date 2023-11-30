a5c8a09d2e9abff8194b8d0f3e64ab566c99cf56
admin <admin@3205EC05>
2023-11-30 14:22:39 +0300

    Добавьте файлы проекта.

---
+++ Smirnovvvv/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Smirnovvvv
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+            cifr.Text += 9;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            cifr.Text+= 1;
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            cifr.Text += 0;
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            cifr.Text += 2;
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            cifr.Text += 3;
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            cifr.Text += 4;
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            cifr.Text += 5;
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            cifr.Text += 6;
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+            cifr.Text += 7;
+        }
+
+        private void Button_Click_11(object sender, RoutedEventArgs e)
+        {
+            cifr.Text += 8;
+        }
+    }
+}
+
